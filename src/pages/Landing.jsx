1457bde11704919aea97ae7c32cb661384c8cce6
Md Abrar Al Zabir <abrarroll2@gmail.com>
2025-05-09 15:19:49 -0500

    Update Landing.jsx

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -8,7 +8,7 @@ const Landing = () => {
     <div className="h-fit w-[100vw] overflow-hidden overscroll-x-none">
       <div>
         <div className="absolute w-full top-[30%] sm:top-1/4 mx-auto z-10">
-          <h1 className="text-white text-3xl sm:text-4xl lg:text-6xl font-extrabold text-center font-oxanium"> 
+          <h1 className="text-white text-2xl sm:text-4xl lg:text-6xl font-extrabold text-center font-oxanium"> 
             <span className="primary-color">I'm a</span>
             <br />
             <TypeAnimation
